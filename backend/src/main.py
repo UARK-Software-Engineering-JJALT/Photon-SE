8df59ae339adb5166abdb508c43988b060a81405
LeoTeeuwen <leoteeuwen325@gmail.com>
2025-09-26 20:47:14 -0500

    websocket payload change

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,7 +8,7 @@ import websockets
 connected_clients = set()
 game_running = True
 
-async def broadcast(message: str):
+async def broadcast(message: dict):
     if connected_clients:
         # The only connected client is going to be the frontend
         await asyncio.gather(*(client.send(message) for client in connected_clients))
@@ -18,16 +18,31 @@ async def handle_client(websocket):
     connected_clients.add(websocket)
     try:
         async for message in websocket:
-            if message == "start":
+            # We are working with a payload that looks like (example):
+            '''
+            {
+                type: "start",
+                message: "12:51"
+            }
+            '''
+            print("Message gotten!: ", message)
+            messageType = message[0]["type"]
+
+            # Assume the input is valid (I know.) and start a task
+            asyncio.create_task()
+            if messageType == "message":
+                await broadcast({"type": "message", "message": "Message gotten."})
+            elif messageType == "start":
                 if not game_running:
                     game_running = True
                     # TODO import and put Tristan's code here that will start the game
                     # TODO Make some normal 
-                    asyncio.create_task()
-                    await broadcast("Game started")
-            elif message == "stop":
+                    await broadcast({"type": "message", "message": "Game Started!"})
+                else:
+                    await broadcast({"type": "message", "message": "Game Already Started!"})
+            elif messageType == "stop":
                 game_running = False
-                await broadcast("Game stopped")
+                await broadcast({"type": "message", "message": "Game Stopped!"})
     finally:
         connected_clients.remove(websocket)
 
