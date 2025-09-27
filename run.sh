dbcadb0b412089aabd5e8844d0a0b68ec7f64258
analog_wizard <thecoolmathdude@gmail.com>
2025-09-26 21:27:19 -0500

    final test

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 source ~/.bashrc
+nvm install 22
 corepack enable pnpm
 apt install pip python3-venv
 python3 -m pip install pipx
@@ -10,6 +11,7 @@ cd ./frontend
 pnpm i
 cd ..
 pipx install poetry
+pipx ensurepath
 cd ./backend
 poetry install
 cd ..
