217b933694254227caddbc39e85b1e9288aae5b8
Jackson Baker <jackson.baker7562@gmail.com>
2025-09-26 19:30:50 -0500

    updated formatting in db.js

--- frontend/app/utils/db.js
+++ frontend/app/utils/db.js
@@ -9,39 +9,39 @@ const client = new Client({
 });
 await client.connect();
 
-const add_player = async (id, player) => {
-  if(id.length === 0) {
+export const add_player = async (id, player) => {
+  if (id.length === 0) {
     return;
   }
   let addition_array = Array.from(id, (id_c, index) => {
     return [id_c, player[index]];
   });
   return await client.query(`INSERT INTO players (id, codename) VALUES ${addition_array.toString()
-                                                                                .replace("[", "(")
-                                                                                .replace("]", ")")
-                                                                                .replace("\n","")};`);
+    .replace("[", "(")
+    .replace("]", ")")
+    .replace("\n", "")};`);
 };
 
-const remove_player = async (id_t) => {
-  let id_array =        Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ != NaN).toString()
-                                                                                      .replace("[", "(")
-                                                                                      .replace("]", ")")
-                                                                                      .replace("\n","");
-  let codename_array =  Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ == NaN).toString()
-                                                                                      .replace("[", "(")
-                                                                                      .replace("]", ")")
-                                                                                      .replace("\n","");
+export const remove_player = async (id_t) => {
+  let id_array = Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ != NaN).toString()
+    .replace("[", "(")
+    .replace("]", ")")
+    .replace("\n", "");
+  let codename_array = Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ == NaN).toString()
+    .replace("[", "(")
+    .replace("]", ")")
+    .replace("\n", "");
   return await client.query(`DELETE FROM players WHERE id IN ${id_array} OR codename IN ${codename_array};`);
 };
 
 const get_player = async (id_t) => {
-  let id_array =        Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ != NaN).toString()
-                                                                                      .replace("[", "(")
-                                                                                      .replace("]", ")")
-                                                                                      .replace("\n","");
-  let codename_array =  Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ == NaN).toString()
-                                                                                      .replace("[", "(")
-                                                                                      .replace("]", ")")
-                                                                                      .replace("\n","");
+  let id_array = Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ != NaN).toString()
+    .replace("[", "(")
+    .replace("]", ")")
+    .replace("\n", "");
+  let codename_array = Array.from(id_t).map(_ => parseInt(_, 0)).filter(_ => _ == NaN).toString()
+    .replace("[", "(")
+    .replace("]", ")")
+    .replace("\n", "");
   return await client.query(`SELECT * FROM players WHERE id IN ${id_array} OR codename IN ${codename_array};`);
 };
