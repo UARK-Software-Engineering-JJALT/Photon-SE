45ee14ede9af93eb5cae7e726adaaed3900a6416
analog_wizard <thecoolmathdude@gmail.com>
2025-09-28 15:58:58 -0500

    Working installation of dependency with a few bodges, but still functional

--- install_deps.sh
+++ install_deps.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-source ~/.bashrc
+git clone https://github.com/nvm-sh/nvm --branch v0.40.3
+bash -c ./nvm/install.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install 22
 corepack enable pnpm
-apt install pip python3-venv
+
+sudo apt install pip python3-venv
 python3 -m pip install pipx
 python3 -m pipx ensurepath
-source ~/.bashrc
+
 cd ./frontend
 pnpm i
 cd ../backend
-pipx install poetry
-pipx ensurepath
-poetry install
+python3 -m pipx install poetry
+python3 -m pipx ensurepath
+~/.local/bin/poetry install
