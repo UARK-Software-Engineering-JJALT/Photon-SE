365c51ddff6b38bf264fbb815abd72695fbc10eb
jaxbkr <jackson.baker7562@gmail.com>
2025-09-30 14:03:30 -0500

    updated install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -7,7 +7,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 22
+nvm install --lts
 corepack enable pnpm
 
 sudo apt install pip python3-venv
